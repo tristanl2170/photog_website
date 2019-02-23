94fdeb34e53258853859af3d722addd6054dc77b
maxell4u <maxell4u@gmail.com>
2019-02-22 21:07:13 -0500

    Added image directory

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+var slideIndex=1;
 window.onload = function(){
-    var slideIndex=1;
-    showSlides(slideIndex)
-
+showSlides(slideIndex)
+}
 function plusSlides(n){
     showSlides(slideIndex+=n);
 }
@@ -10,18 +10,18 @@ function currentSlides(n){
     showSlides(slideIndex=n)
 }
 
-function showSlides(n){
+function showSlides(n) {
     var i;
-    var slides=document.getElementsByClassName("mySlide");
-    var dots=document.getElementsByClassName("dot")
-    if (n>slides.length){slideIndex=1}
-    if (n<1){slideIndex=slides.length}
-    for (i=0;i<slides.length; i++)
-        slides[i].getElementsByClassName.display="none"
-
-    for (i=0, i<dots.length; i++;)
-        dot[i].className=dot[i].className.replace("active","");
-
-    slides[slideIndex-1].style.display="block";
-    dots[slideIndex-1].className="active";}
-}
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
